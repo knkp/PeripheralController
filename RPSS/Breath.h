d04945de02825b8b10e6579364ca2349aea563fd
Stephen Copeland <knkp934@gmail.com>
2015-04-30 19:02:27 -0400

    project had issue's building initially, corrected issues, mostly mispellings or wrong paths

---
+++ RPSS/Breath.h
@@ -0,0 +1,18 @@
+#pragma once
+class Breath
+{
+	friend class Cabinet;
+
+private://can not be accesed
+	int 
+		breath;
+
+public:
+	Breath(void);
+	~Breath(void);
+
+	void DoYourBest(int &breath);
+	void Pass(int &breath);
+	void Faile();
+};
+
