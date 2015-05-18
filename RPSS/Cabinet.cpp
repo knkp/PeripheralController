41a1474cc37b5e5929c232db6f9b3c9ef81e8b71
Jeremy2530 <JColon2530@yahoo.com>
2015-05-18 18:06:03 -0400

    Update Cabinet.cpp

--- RPSS/Cabinet.cpp
+++ RPSS/Cabinet.cpp
@@ -1,5 +1,16 @@
 #include "RPSS.h"
+#include "FingerPrint_Library.h"
+#include "SoftwareSerial.h"
 
+uint8_t getFingerprintEnroll(uint8_t id);
+
+int solenoidControl = 5; // Digital Arduino Pin to control solenoid
+
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
 Cabinet::Cabinet(void)
 {
@@ -8,15 +19,32 @@ Cabinet::Cabinet(void)
 Cabinet::~Cabinet(void)
 {
 }
-
-char Cabinet::openPatronCabinet(){
-    // perform necessary operation to open the correct cabinet
    
-   return RPSS_RELEASED; 
+  
+char Cabinet::openPatronCabinet(){
+  // perform necessary operation to open the correct cabinet
+  // found a match!
+  digitalWrite(solenoidControl, LOW);
+  delay(10000);
+  digitalWrite(solenoidControl, HIGH);
+  //Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  //Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+ return RPSS_RELEASED; 
 }
 
-int Cabinet::get_new_cabinet(){
+
+int Cabinet::get_new_cabinet()
+{
   // find the next available cabinet and open it for the patron, also return the cabinets id so it can be associated
   // with the patron in the database
+  if ()
+{
+  // action A
+}
+else
+{
+  // action B
+}
   int cabinetID;
 }
